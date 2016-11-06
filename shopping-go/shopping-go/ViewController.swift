d40c9ef22d8b8546affcb27217c14e6a3966ffc0
liuhang <380864023@qq.com>
2016-11-06 20:54:36 +0800

    初始化项目

---
+++ shopping-go/shopping-go/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  shopping-go
+//
+//  Created by liuhang on 16/11/6.
+//  Copyright © 2016年 刘航. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
